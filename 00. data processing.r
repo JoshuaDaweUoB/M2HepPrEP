21756eef2909f8d500b0d1d8dfd64ab578fdd66d
JoshuaDaweUoB <joshua.dawe@bristol.ac.uk>
2025-07-08 16:30:19 -0400

    load

--- 00. data processing.r
+++ 00. data processing.r
@@ -5,5 +5,6 @@ pacman::p_load(dplyr, tidyr, readr, readxl, lubridate)
 setwd("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Montreal paper/")
 
 # load raw data
-baseline <- read.csv("data/cleaned_m2_data_05012025 (1).csv")
-View(baseline)
+m2hepprep_raw <- read.csv("data/cleaned_m2_data_05012025 (1).csv")
+View(m2hepprep_raw)
+
