931686d5f60c08c3f858dc989369c049f2a0558c
JoshuaDaweUoB <joshua.dawe@bristol.ac.uk>
2025-09-18 14:57:51 -0400

    added latent class analysis

--- 00. data processing.r
+++ 00. data processing.r
@@ -21,15 +21,19 @@ m2hepprep_consent <- m2hepprep_raw %>%
 # baseline vars
 m2hepprep_baseline_vars <- m2hepprep_raw %>%
   filter(redcap_event_name == "Baseline") %>%
-  select(
+  dplyr::select(
     record_id, aiv_kid_evr_pa, aiv_adt_evr_pa, aiv_6m_pa, aiv_kid_evr_sex, aiv_adt_evr_sex, aiv_6m_sex,
     cla_2, cla_16a, cla_16c, nms_er, nms_hps_drg, nms_otp, nms_rsd, nms_auc, nms_opd, nms_mnt, nms_trp,
-    srb_1m_m, dem_edu, nms_emp, nms_inc, nms_inc_cad, nms_inc_usd, srb_1m_fc_hiv,
+    srb_1m_m, dem_edu, nms_emp, nms_inc, nms_inc_cad, nms_inc_usd, srb_1m_fc_hiv, srb_3m_m,
     nms_opd_med___0, nms_opd_med___1, nms_opd_med___2, nms_opd_med___3, nms_opd_med___4, nms_opd_med___5, nms_opd_med___6, nms_opd_med_ot,
-    odu_6m, dem_hltins, sdu_srg, sub_frq1m, srb_3m_sxp, srb_1m_m_hiv, srb_1m_f_hiv, srb_1m_f_drg_cc, srb_1m_f_drg_hro, srb_1m_f_drg_main, srb_1m_f_drg_aph, srb_1m_f_drg_psy, 
-    sub_6m1, sub_6m2, sub_6m3, sub_6m4, sub_6m5, sub_6m6, sub_6m7, sub_6m8, sub_6m9, sub_6m10, sub_6m11, sub_6m12, sub_6m13, sub_6m14, sub_6m15, sub_6m16, sub_6m17, sub_6m18, sub_6m19, sub_6m20, sub_6m21, sub_6m22, sub_6m23, sub_6m24, sub_6m25, sub_6m26, sub_6m27,
-    sdu_wrk, sdu_wrk_6m_frq, srb_3m,, srb_3m_sxp, idu_6mplc2___3, dem_gender_id, dem_gender,
-    srb_3m_prst, srb_prst04, srb_3m, srb_3m_f, srb_1m_fc_pv, srb_1m_fc_pa, srb_1m_fc_npv, srb_1m_fc_npa, srb_1m_fc_cv, srb_1m_fc_ca, srb_1m_fc_mcv, srb_1m_fc_mco, srb_1m_fc_mca
+    odu_6m, dem_hltins, sdu_srg, sub_frq1m, srb_3m_sxp, srb_1m_m_hiv, srb_1m_f_hiv, srb_1m_f_drg_cc, srb_1m_f_drg_hro, srb_1m_f_drg_main,
+    srb_1m_f_drg_aph, srb_1m_f_drg_psy, 
+    sub_6m1, sub_6m2, sub_6m3, sub_6m4, sub_6m5, sub_6m6, sub_6m7, sub_6m8, sub_6m9, sub_6m10, sub_6m11, sub_6m12, 
+    sub_6m13, sub_6m14, sub_6m15, sub_6m16, sub_6m17, sub_6m18, sub_6m19, sub_6m20, sub_6m21, sub_6m22, sub_6m23, sub_6m24, sub_6m25, sub_6m26, sub_6m27,
+    sdu_wrk, sdu_wrk_6m_frq, srb_3m, idu_6mplc2___3, dem_gender_id, dem_gender,
+    srb_3m_prst, srb_prst04, srb_3m_f, srb_1m_fc_pv, srb_1m_fc_pa, srb_1m_fc_npv, srb_1m_fc_npa, srb_1m_fc_cv, srb_1m_fc_ca, srb_1m_fc_mcv, srb_1m_fc_mco, srb_1m_fc_mca,
+    srb_1m_mc_pv, srb_1m_mc_pa, srb_1m_mc_npv, srb_1m_mc_npa, srb_1m_mc_cv, srb_1m_mc_ca, srb_1m_mc_mcv, srb_1m_mc_mco, srb_1m_mc_mca, srb_1m_f,
+    srb_3m_f_sxp_pidu, srb_3m_f_sxp_npidu, srb_3m_f_sxp_cidu, srb_3m_f_sxp_mcidu, srb_3m_m_sxp_pidu, srb_3m_m_sxp_npidu, srb_3m_m_sxp_cidu, srb_3m_m_sxp_mcidu
   )
 
 # baseline data
@@ -225,7 +229,11 @@ m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
     ),
     msm_ever = factor(
       case_when(
+        dem_gender == "Man" & srb_1m_m == "Yes" ~ "Yes",
+        dem_gender == "Man" & srb_3m_m == "Yes" ~ "Yes",
         dem_gender == "Man" & (dem_gender_id == "Homosexual" | dem_gender_id == "Bisexual") ~ "Yes",
+        dem_gender == "Man" & srb_1m_m == "No" ~ "No",
+        dem_gender == "Man" & srb_3m_m == "No" ~ "No",
         dem_gender == "Man" & (dem_gender_id == "Heterosexual" | dem_gender_id == "Other") ~ "No",
         TRUE ~ NA_character_
       ),
@@ -282,30 +290,25 @@ m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
       ),
       levels = c("No", "Yes")
     ),
-    syringe_loan_bin = factor(
+    syringe_loan_6m_bin = factor(
       case_when(
         (sdem_idu6m___4 == "Let someone else use a needle after you used it" | 
          sdem_idu6m___5 == "Let someone else use the rinse water, etc" | 
          sdem_idu6m___6 == "Allow someone else to inject with drugs") ~ "Yes",
-        sdem_idu6m___1 == "Use a needle that you knew or suspected someone else had used before" |
-        sdem_idu6m___0 == "Reuse a needle without cleaning it with bleach or boiling water first" |
-        sdem_idu6m___2 == "Use someone else's rinse water, cooker, or cotton" | 
-        sdem_idu6m___3 == "Ever skip cleaning your needle with bleach or boiling it after you were done" | 
-        sdem_idu6m___7 == "None of the above / NA" ~ "No",
+        TRUE ~ "No"
+      ),
+      levels = c("No", "Yes")
+    ),
+    syringe_cooker_6m_bin = factor(
+      case_when(
+         sdem_idu6m___2 == "Use someone else's rinse water, cooker, or cotton" ~ "Yes",
         TRUE ~ NA_character_
       ),
       levels = c("No", "Yes")
     ),
-    syringe_other_bin = factor(
+    syringe_reuse_6m_bin = factor(
       case_when(
-        (sdem_idu6m___0 == "Reuse a needle without cleaning it with bleach or boiling water first" |
-         sdem_idu6m___2 == "Use someone else's rinse water, cooker, or cotton" | 
-         sdem_idu6m___3 == "Ever skip cleaning your needle with bleach or boiling it after you were done") ~ "Yes",
-        sdem_idu6m___4 == "Let someone else use a needle after you used it" | 
-        sdem_idu6m___5 == "Let someone else use the rinse water, etc" | 
-        sdem_idu6m___6 == "Allow someone else to inject with drugs" |
-        sdem_idu6m___1 == "Use a needle that you knew or suspected someone else had used before" |
-        sdem_idu6m___7 == "None of the above / NA" ~ "No",
+         sdem_idu6m___0 == "Reuse a needle without cleaning it with bleach or boiling water first" ~ "Yes",
         TRUE ~ NA_character_
       ),
       levels = c("No", "Yes")
@@ -350,7 +353,9 @@ cols_to_na <- c(
   "srb_3m_prst", "srb_prst04", "srb_1m_f_hiv", "sdem_sex", "srb_3m", "srb_1m_fc_hiv", "srb_3m_sxp",
   "srb_1m_f_drg_cc", "srb_1m_f_drg_hro", "srb_1m_f_drg_main", "srb_1m_f_drg_aph", "srb_1m_f_drg_psy",
   "srb_3m_f", "srb_1m_fc_pv", "srb_1m_fc_pa", "srb_1m_fc_npv", "srb_1m_fc_npa", "srb_1m_fc_cv",
-  "srb_1m_fc_ca", "srb_1m_fc_mcv", "srb_1m_fc_mco", "srb_1m_fc_mca", "cla_16a"
+  "srb_1m_fc_ca", "srb_1m_fc_mcv", "srb_1m_fc_mco", "srb_1m_fc_mca", "cla_16a", "srb_3m_f_sxp_pidu",
+  "srb_3m_f_sxp_npidu", "srb_3m_f_sxp_cidu", "srb_3m_f_sxp_mcidu", "srb_3m_m_sxp_pidu", "srb_3m_m_sxp_npidu",
+  "srb_3m_m_sxp_cidu", "srb_3m_m_sxp_mcidu"
 )
 m2hepprep_prep_combined[cols_to_na] <- lapply(
   m2hepprep_prep_combined[cols_to_na],
@@ -399,10 +404,26 @@ m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
       case_when(
         sdem_sex == "Male" & (is.na(srb_3m) | srb_3m == "No") ~ "0",
         sdem_sex == "Male" ~ as.character(srb_1m_fc_hiv),
-        sdem_sex == "Male" & srb_3m != "Yes" ~ "0",
         TRUE ~ NA_character_
       ),
       levels = c("Never", "Rarely", "Some of the time", "Very often", "Always", "0")
+    ),
+    sex_with_pwid_3m = factor(
+      ifelse(
+        rowSums(
+          cbind(
+            as.numeric(srb_3m_f_sxp_pidu) > 0,
+            as.numeric(srb_3m_f_sxp_npidu) > 0,
+            as.numeric(srb_3m_f_sxp_cidu) > 0,
+            as.numeric(srb_3m_f_sxp_mcidu) > 0,
+            as.numeric(srb_3m_m_sxp_pidu) > 0,
+            as.numeric(srb_3m_m_sxp_npidu) > 0,
+            as.numeric(srb_3m_m_sxp_cidu) > 0,
+            as.numeric(srb_3m_m_sxp_mcidu) > 0
+          ), na.rm = TRUE
+        ) > 0, "Yes", "No"
+      ),
+      levels = c("No", "Yes")
     ),
     any_sex_3m = factor(srb_3m, levels = c("No", "Yes")),
     sex_on_cocaine_1m = factor(srb_1m_f_drg_cc, levels = c("Never", "Rarely", "Some of the time", "Very often", "Always")),
@@ -433,15 +454,15 @@ m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
     ),
     condom_1m = factor(
       case_when(
-        srb_1m_fc_pv %in% c("Very often", "Always") |
-        srb_1m_fc_pa %in% c("Very often", "Always") |
-        srb_1m_fc_npv %in% c("Very often", "Always") |
-        srb_1m_fc_npa %in% c("Very often", "Always") |
-        srb_1m_fc_cv %in% c("Very often", "Always") |
-        srb_1m_fc_ca %in% c("Very often", "Always") |
-        srb_1m_fc_mcv %in% c("Very often", "Always") |
-        srb_1m_fc_mco %in% c("Very often", "Always") |
-        srb_1m_fc_mca %in% c("Very often", "Always") ~ "Frequent condom use",
+        srb_1m_mc_pv %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_pa %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_npv %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_npa %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_cv %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_ca %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_mcv %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_mco %in% c("Never", "Rarely", "Some of the time") |
+        srb_1m_mc_mca %in% c("Never", "Rarely", "Some of the time") |
         srb_1m_fc_pv %in% c("Never", "Rarely", "Some of the time") |
         srb_1m_fc_pa %in% c("Never", "Rarely", "Some of the time") |
         srb_1m_fc_npv %in% c("Never", "Rarely", "Some of the time") |
@@ -450,11 +471,36 @@ m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
         srb_1m_fc_ca %in% c("Never", "Rarely", "Some of the time") |
         srb_1m_fc_mcv %in% c("Never", "Rarely", "Some of the time") |
         srb_1m_fc_mco %in% c("Never", "Rarely", "Some of the time") |
-        srb_1m_fc_mca %in% c("Never", "Rarely", "Some of the time") ~ "Infrequent condom use",
-        srb_3m != "Yes" ~ "No sex",
+        srb_1m_fc_mca %in% c("Never", "Rarely", "Some of the time") ~ "Inconsistent condom use",
+        srb_1m_mc_pv %in% c("Very often", "Always") |
+        srb_1m_mc_pa %in% c("Very often", "Always") |
+        srb_1m_mc_npv %in% c("Very often", "Always") |
+        srb_1m_mc_npa %in% c("Very often", "Always") |
+        srb_1m_mc_cv %in% c("Very often", "Always") |
+        srb_1m_mc_ca %in% c("Very often", "Always") |
+        srb_1m_mc_mcv %in% c("Very often", "Always") |
+        srb_1m_mc_mco %in% c("Very often", "Always") |
+        srb_1m_mc_mca %in% c("Very often", "Always") |
+        srb_1m_fc_pv %in% c("Very often", "Always") |
+        srb_1m_fc_pa %in% c("Very often", "Always") |
+        srb_1m_fc_npv %in% c("Very often", "Always") |
+        srb_1m_fc_npa %in% c("Very often", "Always") |
+        srb_1m_fc_cv %in% c("Very often", "Always") |
+        srb_1m_fc_ca %in% c("Very often", "Always") |
+        srb_1m_fc_mcv %in% c("Very often", "Always") |
+        srb_1m_fc_mco %in% c("Very often", "Always") |
+        srb_1m_fc_mca %in% c("Very often", "Always") ~ "Consistent condom use",
+        srb_3m != "Yes" ~ "No sex past 3 months",
+        srb_1m_m == "No" | srb_1m_f == "No" ~ "No sex past month",
+        srb_1m_mc_pv == "Not Applicable" | srb_1m_mc_pa == "Not Applicable" |
+        srb_1m_mc_npa == "Not Applicable" | srb_1m_mc_cv == "Not Applicable" |
+        srb_1m_mc_mcv == "Not Applicable" | srb_1m_mc_mco == "Not Applicable" |
+        srb_1m_fc_pv == "Not Applicable" | srb_1m_fc_pa == "Not Applicable" |
+        srb_1m_fc_npa == "Not Applicable" | srb_1m_fc_cv == "Not Applicable" |
+        srb_1m_fc_mcv == "Not Applicable" | srb_1m_fc_mco == "Not Applicable" ~ NA_character_,
         TRUE ~ NA_character_
       ),
-      levels = c("No condom use", "Condom use", "No sex")
+      levels = c("Consistent condom use", "Inconsistent condom use", "No sex past month", "No sex past 3 months")
     ),
     sex_work_ever = factor(
       case_when(
@@ -468,5 +514,3 @@ m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
 
 # save data
 write.csv(m2hepprep_prep_combined, "data/m2hepprep_combined.csv", row.names = FALSE)
-
-View(m2hepprep_prep_combined)
--- 01. data analysis.r
+++ 01. data analysis.r
@@ -1,5 +1,5 @@
 # load packages
-pacman::p_load(dplyr, tidyr, readr, readxl, lubridate, tableone, broom, lmtest, sandwich, logistf, writexl)
+pacman::p_load(dplyr, tidyr, readr, readxl, lubridate, tableone, broom, lmtest, sandwich, logistf, writexl, poLCA)
 
 # set working directory
 setwd("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Montreal paper/")
@@ -38,6 +38,7 @@ risk_lists <- list(
     "sex_work_ever",
     "sexwork_3m",
     "bought_sex_3m",
+    "msm_ever",
     "sexual_abuse_ever",
     "sexual_abuse_6m",
     "sex_on_any_drug_1m"
@@ -76,6 +77,7 @@ risk_labels <- c(
   sex_work_ever         = "Ever engaged in sex work",
   sexwork_3m            = "Sex work past 3 months",
   bought_sex_3m         = "Bought sex past 3 months",
+  msm_ever              = "Men who have sex with men",
   sexual_abuse_ever     = "Ever experienced sexual abuse",
   sexual_abuse_6m       = "Sexual abuse past 6 months",
   sex_on_any_drug_1m    = "Sex on any drug past 1 month"
@@ -84,6 +86,227 @@ risk_labels <- c(
 # --- Convert prep_init to numeric for Poisson
 m2hepprep_prep_combined <- m2hepprep_prep_combined %>%
   mutate(prep_init_num = ifelse(prep_init == "Yes", 1, 0))
+
+# miami 
+m2hepprep_prep_combined_miami <- m2hepprep_prep_combined %>%
+  dplyr::filter(sdem_reside == "Greater Miami area")
+
+# montreal 
+m2hepprep_prep_combined_montreal <- m2hepprep_prep_combined %>%
+  dplyr::filter(sdem_reside == "Greater Montreal area")
+
+## latent class analysis
+
+# sexual and injecting risk variables
+lca_vars <- c(
+  "inject_heroin_6m", "inject_cocaine_6m", "inject_meth_6m", "inject_fent_6m",
+  "syringe_share_6m_bin", "syringe_loan_6m_bin", "syringe_cooker_6m_bin", "syringe_reuse_6m_bin", "days_used_1m_3cat",
+  "num_sex_partners_3m", "condom_1m", "sexwork_3m", "bought_sex_3m", "sexual_abuse_6m"
+)
+
+# missing values for all LCA variables to "No"
+for (v in lca_vars) {
+  if (v %in% names(m2hepprep_prep_combined)) {
+    # If factor, add "No" as a level if missing
+    if (is.factor(m2hepprep_prep_combined[[v]])) {
+      m2hepprep_prep_combined[[v]] <- forcats::fct_expand(m2hepprep_prep_combined[[v]], "No")
+    }
+    m2hepprep_prep_combined[[v]][is.na(m2hepprep_prep_combined[[v]])] <- "No"
+  }
+}
+
+# lca data data
+lca_data <- m2hepprep_prep_combined %>%
+  dplyr::select(all_of(lca_vars))
+
+# convert to factors
+lca_data[] <- lapply(lca_data, as.factor)
+
+# lca formula
+lca_formula <- as.formula(paste("cbind(", paste(lca_vars, collapse = ","), ") ~ 1"))
+
+# sample size and missingness
+cat("Sample size for LCA:", nrow(lca_data), "\n")
+print(sapply(lca_data, function(x) sum(is.na(x))))
+
+# LCA for 1 to 5 classes
+lca_results <- list()
+fit_stats <- data.frame(
+  NClasses = integer(),
+  NFreeParameters = integer(),
+  AIC = numeric(),
+  BIC = numeric(),
+  SABIC = numeric(),
+  Entropy = numeric(),
+  LL = numeric(),
+  ChiSquare = numeric(),
+  df = integer(),
+  NClass1 = integer(),
+  NClass2 = integer(),
+  NClass3 = integer(),
+  NClass4 = integer(),
+  NClass5 = integer(),
+  stringsAsFactors = FALSE
+)
+
+# set seed
+set.seed(123)
+
+# calculate fit statistics
+for (k in 1:5) {
+  lca_model <- poLCA(lca_formula, data = lca_data, nclass = k, maxiter = 1000, na.rm = TRUE, verbose = FALSE)
+  lca_results[[k]] <- lca_model
+  # SABIC
+  n <- nrow(lca_data)
+  sabic <- lca_model$bic - log(n) * (lca_model$npar - 1) / 2
+  # entropy
+  entropy <- NA
+  if (!is.null(lca_model$posterior)) {
+    p <- lca_model$posterior
+    entropy <- 1 - sum(p * log(p + 1e-10)) / (n * log(k))
+  }
+  # class sizes
+  class_sizes <- table(lca_model$predclass)
+  # clean missing classes
+  class_counts <- rep(NA, 5)
+  class_counts[1:length(class_sizes)] <- as.numeric(class_sizes)
+  fit_stats <- rbind(fit_stats, data.frame(
+    NClasses = k,
+    NFreeParameters = lca_model$npar,
+    AIC = lca_model$aic,
+    BIC = lca_model$bic,
+    SABIC = sabic,
+    Entropy = round(entropy, 2),
+    LL = lca_model$llik,
+    ChiSquare = lca_model$Gsq,
+    df = lca_model$resid.df,
+    NClass1 = class_counts[1],
+    NClass2 = class_counts[2],
+    NClass3 = class_counts[3],
+    NClass4 = class_counts[4],
+    NClass5 = class_counts[5]
+  ))
+}
+
+print(fit_stats)
+write.csv(fit_stats, "data/lca_fit_stats.csv", row.names = FALSE)
+
+best_model <- fit_stats[which.min(fit_stats$BIC), ]
+print(best_model)
+
+# class membership
+lca_data$class <- lca_results[[3]]$predclass
+
+# class proportions and item-response probabilities
+print(lca_results[[3]])
+
+# Ylabel classes as LIHS, HIMS, LILS
+table(lca_data$class)
+
+# add to original dataset
+m2hepprep_prep_combined$class <- NA
+m2hepprep_prep_combined$class[as.numeric(rownames(lca_data))] <- lca_data$class
+
+# tab prep_init by class
+table(m2hepprep_prep_combined$class, m2hepprep_prep_combined$prep_init, useNA = "ifany")
+
+# calc proportions
+prop.table(table(m2hepprep_prep_combined$class, m2hepprep_prep_combined$prep_init), 1)
+
+# Recode class_factor levels to descriptive labels and set class 2 as reference
+m2hepprep_prep_combined$class_factor <- factor(
+  m2hepprep_prep_combined$class,
+  levels = c(1, 2, 3),
+  labels = c("High Injection/Low Sexual Risk", "Low Overall Risk", "Moderate Injection/High Sexual Risk")
+)
+m2hepprep_prep_combined$class_factor <- relevel(m2hepprep_prep_combined$class_factor, ref = "Low Overall Risk")
+
+# Poisson regression: prep_init ~ class
+mod_class <- glm(prep_init_num ~ class_factor + sdem_reside, data = m2hepprep_prep_combined, family = poisson(link = "log"))
+
+# Exponentiate estimates and save results as before
+robust_se <- sqrt(diag(vcovHC(mod_class, type = "HC0")))
+coefs <- coeftest(mod_class, vcov. = vcovHC(mod_class, type = "HC0"))
+exp_coef <- exp(coefs[, "Estimate"])
+conf_low <- exp(coefs[, "Estimate"] - 1.96 * robust_se)
+conf_high <- exp(coefs[, "Estimate"] + 1.96 * robust_se)
+p_val <- coefs[, "Pr(>|z|)"]
+
+poisson_class_results <- data.frame(
+  term = rownames(coefs),
+  estimate = exp_coef,
+  conf.low = conf_low,
+  conf.high = conf_high,
+  p.value = p_val,
+  stringsAsFactors = FALSE
+)
+
+# Class 2: "Low Overall Risk"
+# Class 1: "High Injection/Low Sexual Risk"
+# Class 3: "Moderate Injection/High Sexual Risk"
+
+# Save to Excel as a sheet
+write_xlsx(list("Poisson_class_results" = poisson_class_results), "data/poisson_class_results.xlsx")
+
+# Make sure class membership is a factor
+m2hepprep_prep_combined$class_factor <- factor(m2hepprep_prep_combined$class)
+
+# Fit multinomial logistic regression: sociostructural risks as predictors of class
+mod_multinom <- multinom(class_factor ~ sdem_reside + sdem_sex_binary + sdem_age_binary + sdem_slep6m_binary +
+                           incarc_6m_bin + oat_ever + methadone_current +
+                           bupe_current + healthcare_coverage,
+                         data = m2hepprep_prep_combined)
+
+# Get coefficients and standard errors
+coefs <- summary(mod_multinom)$coefficients
+ses <- summary(mod_multinom)$standard.errors
+
+# Calculate RRRs and 95% CIs
+rrr <- exp(coefs)
+ci_low <- exp(coefs - 1.96 * ses)
+ci_high <- exp(coefs + 1.96 * ses)
+
+# Get p-values
+z <- coefs / ses
+p <- 2 * (1 - pnorm(abs(z)))
+
+# Format as a table
+results_table <- data.frame(
+  Predictor = rep(colnames(coefs), each = nrow(coefs)),
+  Class = rep(rownames(coefs), times = ncol(coefs)),
+  RRR = as.vector(rrr),
+  CI_low = as.vector(ci_low),
+  CI_high = as.vector(ci_high),
+  p_value = as.vector(p)
+)
+
+# Print nicely
+knitr::kable(results_table, digits = 2, caption = "Multinomial regression: Sociostructural risks as predictors of class membership")
+
+# Save Poisson regression results (with interaction) to Excel spreadsheet
+write_xlsx(results_table, "data/lca_predictors.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # --- Helper for descriptive summaries with concatenated x (x.x)
 get_prepinit_summary <- function(var, data) {
@@ -191,6 +414,292 @@ names(arch_regression_results_dfs) <- names(risk_lists)
 
 # Save to Excel with each group as a sheet
 write_xlsx(arch_regression_results_dfs, "data/arch_vars_prepinit_poisson_results.xlsx")
+
+fit_poisson_pr <- function(var, data) {
+  # skip if fewer than 2 levels
+  if(length(unique(na.omit(data[[var]]))) < 2) return(NULL)
+  
+  formula <- as.formula(paste("prep_init_num ~", var))  # <-- no sdem_reside
+  mod <- glm(formula, data = data, family = poisson(link = "log"))
+  
+  robust_se <- sqrt(diag(vcovHC(mod, type = "HC0")))
+  coefs <- coeftest(mod, vcov. = vcovHC(mod, type = "HC0"))
+  
+  df <- data.frame(
+    term = rownames(coefs),
+    estimate = exp(coefs[, "Estimate"]),
+    conf.low = exp(coefs[, "Estimate"] - 1.96 * robust_se),
+    conf.high = exp(coefs[, "Estimate"] + 1.96 * robust_se),
+    p.value = coefs[, "Pr(>|z|)"],
+    stringsAsFactors = FALSE
+  )
+  
+  df <- df %>%
+    mutate(
+      Variable = var,
+      Level = gsub(paste0("^", var), "", term),
+      Level = ifelse(Level == "", levels(data[[var]])[1], Level)
+    ) %>%
+    filter(term != "(Intercept)")
+  
+  return(df)
+}
+
+# --- Main loop (Miami only) ---
+arch_regression_results_dfs_miami <- lapply(names(risk_lists), function(group_name) {
+  
+  out_list <- list()
+  
+  for (var in risk_lists[[group_name]]) {
+    if (var %in% names(m2hepprep_prep_combined_miami)) {
+      
+      # Fit Poisson regression
+      formula <- as.formula(paste("prep_init_num ~", var))
+      mod <- glm(formula, data = m2hepprep_prep_combined_miami, family = poisson(link = "log"))
+      robust_se <- sqrt(diag(vcovHC(mod, type = "HC0")))
+      coefs <- coeftest(mod, vcov. = vcovHC(mod, type = "HC0"))
+      
+      reg_df <- data.frame(
+        term = rownames(coefs),
+        estimate = exp(coefs[, "Estimate"]),
+        conf.low = exp(coefs[, "Estimate"] - 1.96 * robust_se),
+        conf.high = exp(coefs[, "Estimate"] + 1.96 * robust_se),
+        p.value = coefs[, "Pr(>|z|)"],
+        stringsAsFactors = FALSE
+      ) %>%
+        mutate(
+          Variable = var,
+          Level = gsub(paste0("^", var), "", term),
+          Level = ifelse(Level == "", levels(m2hepprep_prep_combined_miami[[var]])[1], Level)
+        ) %>%
+        filter(term != "(Intercept)")
+      
+      # Descriptive summary
+      desc_df <- get_prepinit_summary(var, m2hepprep_prep_combined_miami)
+      
+      # Combine regression and descriptive summaries
+      all_levels <- data.frame(
+        Variable = var,
+        Level = levels(m2hepprep_prep_combined_miami[[var]]),
+        stringsAsFactors = FALSE
+      )
+      
+      combined <- all_levels %>%
+        left_join(
+          reg_df %>%
+            mutate(
+              `PR (95% CI)` = sprintf("%.2f (%.2f-%.2f)", estimate, conf.low, conf.high),
+              `P-value` = ifelse(p.value < 0.001, "<0.001", sprintf("%.3f", p.value))
+            ) %>%
+            select(Variable, Level, `PR (95% CI)`, `P-value`),
+          by = c("Variable", "Level")
+        ) %>%
+        left_join(desc_df, by = c("Variable", "Level")) %>%
+        mutate(
+          `PR (95% CI)` = ifelse(is.na(`PR (95% CI)`), "ref.", `PR (95% CI)`),
+          `P-value` = ifelse(is.na(`P-value`), "", `P-value`)
+        )
+      
+      # Add empty row between variables
+      combined <- rbind(combined, setNames(as.list(rep(NA, ncol(combined))), colnames(combined)))
+      
+      out_list[[var]] <- combined
+    }
+  }
+  
+  bind_rows(out_list)
+})
+
+names(arch_regression_results_dfs_miami) <- names(risk_lists)
+
+# Save results to Excel
+write_xlsx(arch_regression_results_dfs_miami, "data/arch_vars_prepinit_poisson_results_miami.xlsx")
+
+# --- Main loop (Montreal only) ---
+arch_regression_results_dfs_montreal <- lapply(names(risk_lists), function(group_name) {
+  
+  out_list <- list()
+  
+  for (var in risk_lists[[group_name]]) {
+    if (var %in% names(m2hepprep_prep_combined_montreal)) {
+      
+      # Fit Poisson regression
+      formula <- as.formula(paste("prep_init_num ~", var))
+      mod <- glm(formula, data = m2hepprep_prep_combined_montreal, family = poisson(link = "log"))
+      robust_se <- sqrt(diag(vcovHC(mod, type = "HC0"))
+      )
+      coefs <- coeftest(mod, vcov. = vcovHC(mod, type = "HC0"))
+      
+      reg_df <- data.frame(
+        term = rownames(coefs),
+        estimate = exp(coefs[, "Estimate"]),
+        conf.low = exp(coefs[, "Estimate"] - 1.96 * robust_se),
+        conf.high = exp(coefs[, "Estimate"] + 1.96 * robust_se),
+        p.value = coefs[, "Pr(>|z|)"],
+        stringsAsFactors = FALSE
+      ) %>%
+        mutate(
+          Variable = var,
+          Level = gsub(paste0("^", var), "", term),
+          Level = ifelse(Level == "", levels(m2hepprep_prep_combined_montreal[[var]])[1], Level)
+        ) %>%
+        filter(term != "(Intercept)")
+      
+      # Descriptive summary
+      desc_df <- get_prepinit_summary(var, m2hepprep_prep_combined_montreal)
+      
+      # Combine regression and descriptive summaries
+      all_levels <- data.frame(
+        Variable = var,
+        Level = levels(m2hepprep_prep_combined_montreal[[var]]),
+        stringsAsFactors = FALSE
+      )
+      
+      combined <- all_levels %>%
+        left_join(
+          reg_df %>%
+            mutate(
+              `PR (95% CI)` = sprintf("%.2f (%.2f-%.2f)", estimate, conf.low, conf.high),
+              `P-value` = ifelse(p.value < 0.001, "<0.001", sprintf("%.3f", p.value))
+            ) %>%
+            select(Variable, Level, `PR (95% CI)`, `P-value`),
+          by = c("Variable", "Level")
+        ) %>%
+        left_join(desc_df, by = c("Variable", "Level")) %>%
+        mutate(
+          `PR (95% CI)` = ifelse(is.na(`PR (95% CI)`), "ref.", `PR (95% CI)`),
+          `P-value` = ifelse(is.na(`P-value`), "", `P-value`)
+        )
+      
+      # Add empty row between variables
+      combined <- rbind(combined, setNames(as.list(rep(NA, ncol(combined))), colnames(combined)))
+      
+      out_list[[var]] <- combined
+    }
+  }
+  
+  bind_rows(out_list)
+})
+
+names(arch_regression_results_dfs_montreal) <- names(risk_lists)
+
+# Save results to Excel
+write_xlsx(arch_regression_results_dfs_montreal, "data/arch_vars_prepinit_poisson_results_montreal.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Save to Excel with each group as a sheet
+write_xlsx(arch_regression_results_dfs_miami, "data/arch_vars_prepinit_poisson_results_miami.xlsx")
+
+# --- Main loop (Montreal only)
+arch_regression_results_dfs_montreal <- lapply(names(risk_lists), function(group_name) {
+  message("\n--- Processing group: ", group_name, " ---")
+  
+  # Initialize list to store results for each variable
+  out_list <- list()
+  
+  for (var in risk_lists[[group_name]]) {
+    if(var %in% names(m2hepprep_prep_combined_montreal)) {
+      reg_df <- fit_poisson_pr(var, m2hepprep_prep_combined_montreal)
+      desc_df <- get_prepinit_summary(var, m2hepprep_prep_combined_montreal)
+      
+      all_levels <- data.frame(
+        Variable = var,
+        Level = levels(m2hepprep_prep_combined_montreal[[var]]),
+        stringsAsFactors = FALSE
+      )
+      
+      combined <- all_levels %>%
+        left_join(reg_df %>%
+                    mutate(`PR (95% CI)` = sprintf("%.2f (%.2f-%.2f)", estimate, conf.low, conf.high),
+                           `P-value` = ifelse(p.value < 0.001, "<0.001", sprintf("%.3f", p.value))) %>%
+                    select(Variable, Level, `PR (95% CI)`, `P-value`),
+                  by = c("Variable", "Level")) %>%
+        left_join(desc_df, by = c("Variable", "Level")) %>%
+        mutate(
+          `PR (95% CI)` = ifelse(is.na(`PR (95% CI)`), "ref.", `PR (95% CI)`),
+          `P-value` = ifelse(is.na(`P-value`), "", `P-value`)
+        )
+      
+      # Add empty row
+      combined <- rbind(combined, setNames(as.list(rep(NA, ncol(combined))), colnames(combined)))
+      
+      out_list[[var]] <- combined
+    }
+  }
+  
+  # Bind all variables in original order
+  out <- bind_rows(out_list)
+  return(out)
+})
+
+names(arch_regression_results_dfs_montreal) <- names(risk_lists)
+
+# Save to Excel with each group as a sheet
+write_xlsx(arch_regression_results_dfs_montreal, "data/arch_vars_prepinit_poisson_results_montreal.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
